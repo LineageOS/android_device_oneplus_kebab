37e997f5d5cbc180857fe2cac1063e132afd9857
Tao Bao <tbao@google.com>
2018-03-29 11:22:10 -0700

    kebab: Set recovery margin height to avoid clipping
    Change-Id: Iaaaabd13de384a0ebfff9f527fcdd0338c3f99bc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,7 @@ BOARD_SUPER_PARTITION_SIZE := 7516192768
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_UI_MARGIN_HEIGHT := 104
 
 # inherit from the proprietary version
 -include vendor/oneplus/kebab/BoardConfigVendor.mk
