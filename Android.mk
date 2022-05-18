fd45a8cb37fc6c5062bf1714d0821b6d036946db
Alexander Koskovich <akoskovich@pm.me>
2022-05-18 08:43:25 -0700

    kebab: Drop comments no longer applicable
    Change-Id: I41b8450cc197cc2faf24b5f3853c341f335979da

--- Android.mk
+++ Android.mk
@@ -4,15 +4,4 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-#
-# This contains the module build definitions for the hardware-specific
-# components for this device.
-#
-# As much as possible, those components should be built unconditionally,
-# with device-specific names to avoid collisions, to avoid device-specific
-# bitrot and build breakages. Building a component unconditionally does
-# *not* include it on all devices, so it is safe even with hardware-specific
-# components.
-#
-
 LOCAL_PATH := $(call my-dir)
