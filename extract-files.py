649ab3dcbb03c8c99ea0bf16ad3ca1eb73b58f7c
LuK1337 <priv.luk@gmail.com>
2024-10-15 22:12:50 +0200

    kebab: Switch to py extract utils
    Change-Id: I385df1ce3d67b291c7b6a543cb6581d2fc60b443

---
+++ extract-files.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/oneplus/sm8250-common',
+]
+
+module = ExtractUtilsModule(
+    'kebab',
+    'oneplus',
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm8250-common', module.vendor
+    )
+    utils.run()
