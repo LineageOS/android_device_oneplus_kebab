533c498e1df893fc433743d6f4dfd18e31aa8435
Bruno Martins <bgcngm@gmail.com>
2026-01-19 00:21:31 +0000

    kebab: Create standalone RRO for KeyHandler
    Change-Id: Ia6867ce4443de4edfd54dc61db5550fc3a59ccd8

--- device.mk
+++ device.mk
@@ -35,6 +35,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    KeyHandlerResTarget \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
